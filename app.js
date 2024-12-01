d084de11a29067b8aeeb13ccc877e693ba212a1b
Konain Raza <131591948+Konain-Raza@users.noreply.github.com>
2024-12-01 22:34:21 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const PORT = process.env.PORT || 3000;
 const app = express();
+const cors = require("cors"); 
 const Routes = require("./routes/routes");
 
 app.use(express.json());
 app.use("/", Routes);
+app.use(cors());  
+
 
 const startServer = () => {
   app.listen(PORT, () => {
